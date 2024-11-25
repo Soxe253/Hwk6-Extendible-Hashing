3111bcf0320de62d0df84c945cf29a1fb09cbfed
Soxe253 <soxe@pugetsound.edu>
2024-11-24 19:04:13 -0800

    Bucket prob done

--- Bucket.java
+++ Bucket.java
@@ -1,22 +1,49 @@
 public class Bucket {
-    String bitDepth;
+    int bitDepth;
     String address;
     int bSize;
     String[] tuples;
+    String bAddress;
 
-    public Bucket(String depth, String addr, int size){
+    public Bucket(int depth, String addr, int size){
         bitDepth = depth;
         address = addr;
         bSize = size;
         tuples = new String[bSize];
+        bAddress = address.substring(0, bitDepth) + "*";
     }
 
-    public void setDepth(String depth){
+    public void setDepth(int depth){
         bitDepth = depth;
     }
 
     public void setAddr(String addr){
         address = addr;
+    }
+
+    public void setBucketAddress(String addr){
+        bAddress = addr;
+    }
+
+    public String toString(){
+        String result = "[";
+        for(int i = 0; i < tuples.length; i++){
+            if(i == (tuples.length - 1)){
+                result = result + tuples[i] + "]";
+            }
+            else{
+            result = result + tuples[i] + ", ";
+            }
+        }
+        return "Local("+bitDepth+")["+bAddress+"] = " + result;
+        
+    }
+
+    public static void main(String[] args){
+        Bucket bucket = new Bucket(1, "10", 2);
+        bucket.tuples[0] = "1000";
+        bucket.tuples[1] = "1010";
+        System.out.println(bucket.toString());
     }
     
 }
--- ExtHash.java
+++ ExtHash.java
@@ -1,8 +1,18 @@
 public class ExtHash{
+int bitDepth;
+Node[] list;
+int keyLength;
+int bucketSize;
 
-
+    public ExtHash(int kLength){
+        bitDepth = 0;
+        keyLength= kLength;
+        bucketSize = 0;
+        list = new Node[1];
+    }
 
     public static void main(String[] args){
-        System.out.println("hello");
+        String[] test = {"hi", "hello"};
+        System.out.println(test);
     }
 }
